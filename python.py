34b18fcf1f71e72af67ed532daa1a681ec9635b0
KlLL4ME <sashavls0706@gmail.com>
2025-09-18 21:59:05 +0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,19 @@
+# Массив
+print("Массив:")
+numbers = [10, 20, 30]
+numbers.append(40)
+print(numbers)
+
+# Список
+print("\nСписок:")
+my_list = [1, "два", 3.0, True]
+print(my_list)
+
+# Стек
+print("\nСтек:")
+stack = []
+stack.append("первый")
+stack.append("второй")
+print("Верхний:", stack[-1])
+stack.pop()
+print("Теперь верхний:", stack[-1])
