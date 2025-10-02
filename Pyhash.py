be25be3701b0fd6e2e21d2463bd4575e76d016fa
KlLL4ME <sashavls0706@gmail.com>
2025-10-02 22:06:34 +0300

    Update Pyhash.py

--- Pyhash.py
+++ Pyhash.py
@@ -1,4 +1,4 @@
 table = {}
-table["Alice"] = 10       # вставка
-v = table["Alice"]        # доступ
-del table["Alice"]        # удаление
+table["Alice"] = 10
+v = table["Alice"]
+del table["Alice"]
